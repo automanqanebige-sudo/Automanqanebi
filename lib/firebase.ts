755203aa890dd0b2853a7549f9712a63e13c9464
automanqanebige-sudo <automanqanebi.ge@gmail.com>
2026-04-28 09:46:46 +0400

    Create firebase.ts

---
+++ lib/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbN4ROdD_TAmmd4BmmzW6OLf8B3sv8S2M",
+  authDomain: "aauuttooo-9c5e6.firebaseapp.com",
+  projectId: "aauuttooo-9c5e6",
+  storageBucket: "aauuttooo-9c5e6.firebasestorage.app",
+  messagingSenderId: "212304817546",
+  appId: "1:212304817546:web:7fecc147ca2cc43639ec73",
+  measurementId: "G-99VPPV3CGF",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
