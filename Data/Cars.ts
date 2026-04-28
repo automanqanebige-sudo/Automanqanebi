c9e32430f80bb2a5c1c67e62cab8c2a16f95b99e
automanqanebige-sudo <automanqanebi.ge@gmail.com>
2026-04-28 21:41:51 +0400

    Create Cars.ts

---
+++ Data/Cars.ts
@@ -0,0 +1,156 @@
+export const carBrands = [
+  {
+    brand: "Toyota",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/9/9d/Toyota_carlogo.svg",
+
+    models: [
+      "Camry",
+      "Corolla",
+      "Prius",
+      "RAV4",
+      "Highlander",
+      "Land Cruiser",
+      "Yaris",
+      "Aqua",
+      "Vitz",
+      "CHR",
+    ],
+  },
+
+  {
+    brand: "BMW",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/4/44/BMW.svg",
+
+    models: [
+      "320",
+      "325",
+      "330",
+      "520",
+      "528",
+      "530",
+      "535",
+      "X3",
+      "X5",
+      "X6",
+    ],
+  },
+
+  {
+    brand: "Mercedes-Benz",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/9/90/Mercedes-Logo.svg",
+
+    models: [
+      "C-Class",
+      "E-Class",
+      "S-Class",
+      "CLS",
+      "GLA",
+      "GLC",
+      "GLE",
+      "ML",
+      "Vito",
+      "Sprinter",
+    ],
+  },
+
+  {
+    brand: "Honda",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/7/7b/Honda-logo.svg",
+
+    models: [
+      "Civic",
+      "Accord",
+      "Fit",
+      "CR-V",
+      "HR-V",
+      "Insight",
+      "Pilot",
+      "Odyssey",
+    ],
+  },
+
+  {
+    brand: "Hyundai",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/4/44/Hyundai_Motor_Company_logo.svg",
+
+    models: [
+      "Elantra",
+      "Sonata",
+      "Santa Fe",
+      "Tucson",
+      "Accent",
+      "Kona",
+    ],
+  },
+
+  {
+    brand: "Kia",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/1/13/Kia-logo.svg",
+
+    models: [
+      "Optima",
+      "Sorento",
+      "Sportage",
+      "Rio",
+      "Cerato",
+      "Soul",
+    ],
+  },
+
+  {
+    brand: "Nissan",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/2/23/Nissan_2020_logo.svg",
+
+    models: [
+      "Altima",
+      "Sentra",
+      "Maxima",
+      "Rogue",
+      "X-Trail",
+      "Juke",
+      "Leaf",
+    ],
+  },
+
+  {
+    brand: "Ford",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/3/3e/Ford_logo_flat.svg",
+
+    models: [
+      "Fusion",
+      "Escape",
+      "Focus",
+      "Mustang",
+      "Explorer",
+      "Transit",
+    ],
+  },
+
+  {
+    brand: "Chevrolet",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/4/4e/Chevrolet-logo.png",
+
+    models: [
+      "Malibu",
+      "Cruze",
+      "Camaro",
+      "Tahoe",
+      "Equinox",
+      "Spark",
+    ],
+  },
+
+  {
+    brand: "Lexus",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/9/9d/Lexus_division_emblem.svg",
+
+    models: [
+      "RX350",
+      "GX460",
+      "LX570",
+      "ES350",
+      "IS250",
+      "NX300",
+    ],
+  },
+];
