fa1dcd7fe86c9e9dc083f2d7db5086cbe69b349e
automanqanebige-sudo <automanqanebi.ge@gmail.com>
2026-04-28 19:32:39 +0400

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,15 +1,25 @@
 import { initializeApp } from "firebase/app";
+
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBbN4ROdD_TAmmd4BmmzW6OLf8B3sv8S2M",
-  authDomain: "aauuttooo-9c5e6.firebaseapp.com",
-  projectId: "aauuttooo-9c5e6",
-  storageBucket: "aauuttooo-9c5e6.firebasestorage.app",
-  messagingSenderId: "212304817546",
-  appId: "1:212304817546:web:7fecc147ca2cc43639ec73",
+
+  apiKey: "აქ შენი apiKey",
+
+  authDomain: "აქ authDomain",
+
+  projectId: "აქ projectId",
+
+  storageBucket: "აქ storageBucket",
+
+  messagingSenderId: "აქ messagingSenderId",
+
+  appId: "აქ appId",
+
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+export default app;
