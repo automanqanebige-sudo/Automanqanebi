9ecb281971aea29fdfeb38b0df9bce74853719ae
automanqanebige-sudo <automanqanebi.ge@gmail.com>
2026-04-28 08:29:43 +0400

    Update layout.tsx

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -10,15 +10,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ka">
-      <body
-        style={{
-          margin: 0,
-          padding: 0,
-          background: "#111",
-        }}
-      >
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
