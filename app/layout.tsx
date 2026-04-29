4bf9c64a1e9b7107846373e209b3160daf38aab3
automanqanebige-sudo <automanqanebi.ge@gmail.com>
2026-04-29 19:39:25 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,10 @@
-import type { Metadata } from "next";
-import "./globals.css"; // ეს ხაზი აუცილებელია Tailwind-ისთვის!
+import "./globals.css"; // ეს უნდა იყოს პირველი!
 import Navbar from "../components/Navbar";
 
-export const metadata: Metadata = {
-  title: "AUTOMANQANEBI",
-  description: "საუკეთესო ავტო პლატფორმა",
-};
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ka">
-      <body className="antialiased bg-gray-50">
+      <body>
         <Navbar />
         {children}
       </body>
