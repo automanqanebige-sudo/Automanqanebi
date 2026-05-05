0ba51c77b42e44334644e80086aa2d7878bb69c7
automanqanebige-sudo <automanqanebi.ge@gmail.com>
2026-05-05 12:04:03 +0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,10 +4,21 @@ const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}", // თუ pages დირექტორიაც გაქვს
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#2563eb",   // ლურჯი
+        dark: "#0a0a0a",      // ფონისთვის
+        card: "#111111",      // card background
+      },
+      borderRadius: {
+        xl2: "1rem",
+      },
+    },
   },
   plugins: [],
 };
+
 export default config;
